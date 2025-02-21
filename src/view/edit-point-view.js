5cc731c821827e9bedc01f6da9fab1a562e4abd0
cooperfirewalk <cooperfirewalk@gmail.com>
2025-02-21 23:36:56 +0300

    добавляет заготовку под картинки

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -137,10 +137,12 @@ function createEditNewPointTemplate(point, offers, destinations) {
                 </header>
                 <section class="event__details">
                 ${pointOffers.length ? editOffersTemplate : ''}
-                  <section class="event__section  event__section--destination">
+                ${selectedDestination?.description.length || selectedDestination?.pictures.length ? `
+                                    <section class="event__section  event__section--destination">
                     <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-                    <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
+                    <p class="event__destination-description">${selectedDestination.description}</p>
                   </section>
+                  ` : ''}
                 </section>
               </form>
             </li>
