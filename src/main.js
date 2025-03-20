022d3306a027020d16788020016faf0716b7d05d
cooperfirewalk <cooperfirewalk@gmail.com>
2025-03-20 15:58:10 +0300

    Добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
-import {render} from './framework/render.js';
-import FiltersView from './view/filters-view.js';
+// import {render} from './framework/render.js'; //для отрисовки кнопки новой задачи
 import EventsPresenter from './presenter/events-presenter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 import EventsModel from './model/events-model.js';
 import FilterModel from './model/filter-model.js';
-
-const filters = [
-  {
-    type: 'everything',
-    count: 0,
-  },
-];
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const tripEventsContainer = document.querySelector('.trip-events');
@@ -19,11 +12,14 @@ const eventsPresenter = new EventsPresenter({
   eventsContainer: tripEventsContainer,
   eventsModel
 });
+const filterPresenter = new FilterPresenter({
+  filterContainer: filtersContainer,
+  filterModel,
+  eventsModel
+});
 
-render(new FiltersView({
-  filters,
-  currentFilterType: 'everything',
-  onFilterTypeChange: () => {}
-}), filtersContainer);
+// в демо проекте здесь отрисовка кнопки новой задачи
+
+filterPresenter.init();
 eventsPresenter.init();
 
---
+++ src/presenter/filter-presenter.js
@@ -0,0 +1,61 @@
+import {render, replace, remove} from '../framework/render.js';
+import FiltersView from '../view/filters-view.js';
+import {filter} from '../utils/filter.js';
+import {FilterType, UpdateType} from '../const.js';
+
+export default class FilterPresenter {
+  #filterContainer = null;
+  #filterModel = null;
+  #eventsModel = null;
+
+  #filterComponent = null;
+
+  constructor({filterContainer, filterModel, eventsModel}) {
+    this.#filterContainer = filterContainer;
+    this.#filterModel = filterModel;
+    this.#eventsModel = eventsModel;
+
+    this.#eventsModel.addObserver(this.#handleModelEvent);
+    this.#filterModel.addObserver(this.#handleModelEvent);
+  }
+
+  get filters() {
+    const points = this.#eventsModel.points;
+
+    return Object.values(FilterType).map((type) => ({
+      type,
+      count: filter[type](points).length
+    }));
+  }
+
+  init() {
+    const filters = this.filters;
+    const prevFilterComponent = this.#filterComponent;
+
+    this.#filterComponent = new FiltersView({
+      filters,
+      currentFilterType: this.#filterModel.filter,
+      onFilterTypeChange: this.#handleFilterTypeChange
+    });
+
+    if (prevFilterComponent === null) {
+      render(this.#filterComponent, this.#filterContainer);
+      return;
+    }
+
+    replace(this.#filterComponent, prevFilterComponent);
+    remove(prevFilterComponent);
+  }
+
+  #handleModelEvent = () => {
+    this.init();
+  };
+
+  #handleFilterTypeChange = (filterType) => {
+    if (this.#filterModel.filter === filterType) {
+      return;
+    }
+
+    this.#filterModel.setFilter(UpdateType.MAJOR, filterType);
+  };
+}
