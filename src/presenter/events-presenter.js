48d8230a94107a1d35a43c4bf2e91684986615dd
cooperfirewalk <cooperfirewalk@gmail.com>
2026-01-13 12:16:18 +0300

    добавляет в модель и презентер приватные поля

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -2,27 +2,34 @@ import TripEventsListView from '../view/trip-events-list-view.js';
 import TripSortView from '../view/trip-sort-view.js';
 import EditPointView from '../view/edit-point-view.js';
 import PointView from '../view/point-view.js';
-import {render} from '../framework/render.js';
+import { render } from '../framework/render.js';
 
 export default class EventsPresenter {
-  listComponent = new TripEventsListView();
+  #eventsContainer = null;
+  #eventsModel = null;
+
+  #listComponent = new TripEventsListView();
+
+  #eventsPoints = [];
+  #offers = null;
+  #destinations = null;
 
   constructor({ eventsContainer, eventsModel }) {
-    this.eventsContainer = eventsContainer;
-    this.eventsModel = eventsModel;
+    this.#eventsContainer = eventsContainer;
+    this.#eventsModel = eventsModel;
   }
 
   init() {
-    this.eventsPoints = [...this.eventsModel.getPoints()];
-    this.offers = this.eventsModel.getOffers();
-    this.destinations = this.eventsModel.getDestinations();
+    this.#eventsPoints = [...this.#eventsModel.points];
+    this.#offers = this.#eventsModel.offers;
+    this.#destinations = this.#eventsModel.destinations;
 
-    render(new TripSortView(), this.eventsContainer);
-    render(this.listComponent, this.eventsContainer);
-    render(new EditPointView({ point: this.eventsPoints[0], offers: this.offers, destinations: this.destinations }), this.listComponent.element);
+    render(new TripSortView(), this.#eventsContainer);
+    render(this.#listComponent, this.#eventsContainer);
+    render(new EditPointView({ point: this.#eventsPoints[0], offers: this.#offers, destinations: this.#destinations }), this.#listComponent.element);
 
     for (let i = 1; i < 4; i++) {
-      render(new PointView({ point: this.eventsPoints[i], offers: this.offers, destinations: this.destinations }), this.listComponent.element);
+      render(new PointView({ point: this.#eventsPoints[i], offers: this.#offers, destinations: this.#destinations }), this.#listComponent.element);
     }
   }
 }
